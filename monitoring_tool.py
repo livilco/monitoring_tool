7c3d175f115ea70d579d9221da68f3c00915112c
kklivil <kushagra@livil.co>
2024-08-27 07:49:54 +0000

    Initial implementation.

---
+++ monitoring_tool.py
@@ -0,0 +1,168 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import logging
+from var import summary_input, pickup_intent_input, smart_reply_input
+import time
+import slack
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename='app.log',
+    filemode='a'  
+)
+
+timeout=120
+#url = "https://mia-staging.livil.co"
+url = "https://us.mia.livil.co"
+slack_channel = "nna_mia_alerts"
+slack_nna_alert_token = os.environ.get("SLACK_NNA_ALERT_TOKEN")
+username = os.environ.get("USER_NAME")
+password = os.environ.get("PASSWORD")
+
+client = slack.WebClient(slack_nna_alert_token)
+
+def send_request_to_mia(
+    data,
+    task: str,
+):
+    endpoint = "/insights"
+    full_url = url + endpoint
+    result = {"is_success": False, 'error_message': "Default"}
+
+    try:
+        response = requests.post(full_url, auth=HTTPBasicAuth(username, password), json=data, timeout=timeout)
+        response.raise_for_status()
+        if response.status_code == 200:
+            logging.info(f"Response: {response.json()}")
+            print(f"Response: {response.json()}")
+            result['is_success'] = True
+            result['error_message'] = ""
+            return result
+
+    except requests.exceptions.HTTPError as e:
+        logging.critical(f"HTTP Error happend for {task} and exception {str(e)}")
+        result['is_success'] = False
+        result['error_message'] = str(e)
+        return result
+
+    except requests.exceptions.ConnectionError as e:
+        logging.critical(f"HTTP Connection error happend for {task} and exception {str(e)}")
+        result['is_success'] = False
+        result['error_message'] = str(e)
+        return result
+
+    except requests.exceptions.Timeout as e:
+        logging.critical(f"Timeout error happend for {task} and exception {str(e)}")
+        result['is_success'] = False
+        result['error_message'] = str(e)
+        return result
+
+    except requests.exceptions.RequestException as e:
+        logging.critical(f"Timeout error happend for {task} and exception {str(e)}")
+        result['is_success'] = False
+        result['error_message'] = str(e)
+        return result
+
+
+def check_summarization(model, task):
+    req_id = 1
+    data = {
+            "req_jobs": [
+                {
+                    "id": req_id,
+                    "input_text": summary_input,
+                    "model": model,
+                    "task": task
+                }
+            ]
+        }
+
+
+    return send_request_to_mia(data, task)
+
+
+def check_pickup_intent(model, task):
+    req_id = 1
+    data = {
+            "req_jobs": [
+                {
+                    "id": req_id,
+                    "input_text": pickup_intent_input,
+                    "model": model,
+                    "task": task
+                }
+            ]
+        }
+
+
+    return send_request_to_mia(data, task)
+
+
+def check_smart_reply(model, task):
+    req_id = 1
+    data = {
+            "req_jobs": [
+                {
+                    "id": req_id,
+                    "input_text": smart_reply_input,
+                    "model": model,
+                    "task": task
+                }
+            ]
+        }
+
+
+    return send_request_to_mia(data, task)
+
+
+def send_slack_notification(error_message):
+    try:
+        client.chat_postMessage(channel=slack_channel, text=error_message)
+    except slack.errors.SlackApiError as e:
+        if e.response['error'] == 'not_in_channel':
+            # Attempt to join the channel first
+            try:
+                client.conversations_join(channel=slack_channel)
+                client.chat_postMessage(channel=slack_channel, text=error_message)
+            except slack.errors.SlackApiError as join_error:
+                print(f"Failed to join and send message: {join_error.response['error']}")
+        else:
+            print(f"Slack API Error: {e.response['error']}")
+
+
+def start_monitoring(sleep):
+    while True:
+        result = check_summarization("jupiter-1", "summarization")
+
+        if not result['is_success']:
+            message = f"Summarization is not working: {result['error_message']} "
+            send_slack_notification(message)
+
+        time.sleep(sleep)
+
+        output = check_smart_reply("jupiter-2", "smart-reply")
+        if not output['is_success']:
+            message = f"Smart reply is not working: {output['error_message']} "
+            send_slack_notification(message)
+
+        time.sleep(sleep)
+
+        result = check_pickup_intent("jupiter-2", "pickup-intent")
+        if not result['is_success']:
+            message = f"Pickup-intent is not working: {result['error_message']} "
+            send_slack_notification(message)
+
+        # wait
+        time.sleep(sleep)
+
+
+#send_slack_notification("This is a test message")
+
+start_monitoring(600)
+
---
+++ var.py
@@ -0,0 +1,8 @@
+
+summary_input = """Over the weekend, I spent some time visiting a local farmers’ market. The variety of fresh produce was incredible, and I ended up buying more than I planned—everything from vibrant tomatoes to fragrant herbs. It was nice to chat with the farmers and learn about where the food comes from. I even found a small bakery stand that had the most delicious bread. There's something special about supporting local businesses and enjoying seasonal foods. Later, I used some of the ingredients to make a simple salad, and the flavors were amazing. I’m thinking of making this a weekly tradition. Have you ever explored a farmers' market in your area? """
+
+
+pickup_intent_input = "Hello John, Could you please buy some oranges and apples."
+
+
+smart_reply_input = "Hello John, How are you doing?"
