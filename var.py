8196ed187c9246cb2a5c0368f6b5fb43432fcaaa
kklivil <kushagra@livil.co>
2025-03-28 13:46:38 +0000

    monitoring support for the new endpoints.

--- var.py
+++ var.py
@@ -1,8 +1,18 @@
 
 summary_input = """Over the weekend, I spent some time visiting a local farmers’ market. The variety of fresh produce was incredible, and I ended up buying more than I planned—everything from vibrant tomatoes to fragrant herbs. It was nice to chat with the farmers and learn about where the food comes from. I even found a small bakery stand that had the most delicious bread. There's something special about supporting local businesses and enjoying seasonal foods. Later, I used some of the ingredients to make a simple salad, and the flavors were amazing. I’m thinking of making this a weekly tradition. Have you ever explored a farmers' market in your area? """
 
-
 pickup_intent_input = "Hello John, Could you please buy some oranges and apples."
 
-
 smart_reply_input = "Hello John, How are you doing?"
+
+email_smart_reply_input = "Dear Jack, How are you? Can we please meet to discuss Q3 results. Regards, Jill."
+
+message_generation_input = "write an message to carry the bat to the stadium."
+
+message_improvisation_input = "Ask Lisa for movie on Friday."
+
+email_generation_input = "write an email to Jhon asking for 2 days leaves."
+
+email_improvisation_input = "Notify a vendor that we’ve decided not to renew their contract after this quarter."
+
+# Summarization, Message Smart reply, Email Smart reply, Message generation, Email generation
